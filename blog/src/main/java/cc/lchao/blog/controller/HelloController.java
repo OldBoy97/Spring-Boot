a89dbae448b4ea9013736804fee2d18990384a81
LianQianChao <63245048+LianQianChao@users.noreply.github.com>
2020-11-24 21:09:27 +0800

    #1 实现HelloWorld功能！ (#2)

---
+++ blog/src/main/java/cc/lchao/blog/controller/HelloController.java
@@ -0,0 +1,17 @@
+package cc.lchao.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class HelloController {
+
+    @GetMapping(value = "/hello")
+    public String hello(){
+        return "Hello World! Spring-Boot!";
+    }
+
+}
